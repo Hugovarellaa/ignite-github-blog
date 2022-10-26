c051b182631a33ec4c8ea900ab87f74ee2c665e6
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-10-26 11:35:19 -0300

    Profile finish

--- src/pages/Home/components/Profile/index.tsx
+++ src/pages/Home/components/Profile/index.tsx
@@ -1,3 +1,6 @@
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faBuilding, faUserGroup } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ExternalLink } from "../../../../components/ExternalLink";
 import { ProfileContainer, ProfileDetails, ProfilePicture } from "./styles";
 
@@ -12,6 +15,24 @@ export function Profile() {
           <ExternalLink text="Github" href="#" />
         </header>
 
+        <p>
+          Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat pulvinar vel mass.
+        </p>
+
+        <ul>
+          <li>
+            <FontAwesomeIcon icon={faGithub} />
+            <span>Hugo Alves</span>
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faBuilding} />
+            Rocketseat
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faUserGroup} />
+            50 seguidores
+          </li>
+        </ul>
       </ProfileDetails>
     </ProfileContainer>
   )
