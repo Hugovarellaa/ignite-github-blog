871eeb1dd845aff3d1bb84cb134ed24c82deb435
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-10-26 15:58:53 -0300

    Exibindo posts do github na tela

--- src/pages/Home/components/Posts/index.tsx
+++ src/pages/Home/components/Posts/index.tsx
@@ -1,22 +1,20 @@
+import { IPost } from "../..";
 import { PostsContainer } from "./styles";
 
-export function Posts() {
+interface IPostProps {
+  post: IPost
+}
+
+export function Posts({ post }: IPostProps) {
   return (
-    <PostsContainer to='/post/1'>
+    <PostsContainer to={`/post/${post.number}`}>
       <div>
-        <strong>JavaScript data types and data structures</strong>
-        <time>Há 1 dia</time>
+        <strong>{post.title}</strong>
+        <time>{post.created_at}</time>
       </div>
 
       <p>
-        Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available in JavaScript and what properties they have. These can be used to build other data structures. Wherever possible, comparisons with other languages are drawn.
-
-        Dynamic typing
-        JavaScript is a loosely typed and dynamic language. Variables in JavaScript are not directly associated with any particular value type, and any variable can be assigned (and re-assigned) values of all types:
-
-        let foo = 42; // foo is now a number
-        foo = 'bar'; // foo is now a string
-        foo = true; // foo is now a boolean
+        {post.body}
       </p>
 
     </PostsContainer>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,50 @@
+import { useCallback, useEffect, useState } from "react";
+import { api } from "../../services/axios";
 import { Posts } from "./components/Posts";
 import { Profile } from "./components/Profile";
 import { SearchInput } from "./components/SearchInput";
 import { HomeContainer } from "./styles";
 
-interface IPost {
+
+const username = import.meta.env.VITE_GITHUB_USERNAME;
+const repoName = import.meta.env.VITE_GITHUB_REPONAME;
+
+
+export interface IPost {
   title: string
+  body: string;
+  created_at: string
+  number: number;
+  html_url: string
+  comments: number;
+  user: {
+    login: string
+  }
 }
 
 export function Home() {
+  const [posts, setPosts] = useState<IPost[]>([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  const getPosts = useCallback(
+    async (query: string = "") => {
+      try {
+        setIsLoading(true)
+        const response = await api.get(
+          `/search/issues?q=${query}%20label:published%20repo:${username}/${repoName}`
+        );
+        console.log(response.data)
+        setPosts(response.data.items);
+      } finally {
+        setIsLoading(false)
+      }
+    },
+    [posts]
+  );
+
+  useEffect(() => {
+    getPosts()
+  }, [])
 
   return (
     <>
@@ -15,13 +52,11 @@ export function Home() {
       <SearchInput />
 
       <HomeContainer>
-        <Posts />
-        <Posts />
-        <Posts />
-        <Posts />
-        <Posts />
-        <Posts />
-        <Posts />
+        {
+          posts.map((post: IPost) => (
+            <Posts key={post.number} post={post} />
+          ))
+        }
       </HomeContainer>
     </>
   )
