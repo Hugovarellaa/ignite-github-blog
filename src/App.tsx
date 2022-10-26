286dab766b99c29166b4ca40ae433d16ea128d2c
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-10-26 10:25:10 -0300

    React-router-dom confg

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Router";
+// import { defaultTheme } from "./styles/theme/defaultTheme";
+
 export function App() {
   return (
-    <div>
-      <h1>Ola mundo</h1>
-    </div>
+    // <ThemeProvider theme={defaultTheme}>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+    // </ThemeProvider>
   )
 }
