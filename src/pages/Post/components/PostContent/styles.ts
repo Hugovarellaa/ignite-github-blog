3a9d8a6cb2c357a00fd400d84bd376c2be39b58c
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-10-26 17:51:54 -0300

    Eslint + prettier

---
+++ src/pages/Post/components/PostContent/styles.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+// eslint- prettier/prettier
+import styled from 'styled-components';
+
+export const PostContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 2.5rem 2rem;
+  margin-bottom: 8rem;
+`;
