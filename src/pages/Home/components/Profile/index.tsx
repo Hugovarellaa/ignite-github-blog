239af9183538ed20deab090f81d51a93dc6dcea7
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-10-26 11:17:53 -0300

    Component External Link

--- src/pages/Home/components/Profile/index.tsx
+++ src/pages/Home/components/Profile/index.tsx
@@ -1,9 +1,18 @@
-import { ProfileContainer, ProfilePicture } from "./styles";
+import { ExternalLink } from "../../../../components/ExternalLink";
+import { ProfileContainer, ProfileDetails, ProfilePicture } from "./styles";
 
 export function Profile() {
   return (
     <ProfileContainer>
       <ProfilePicture src="https://github.com/Hugovarellaa.png" />
+
+      <ProfileDetails>
+        <header>
+          <h1>Hugo Varella</h1>
+          <ExternalLink text="Github" href="#" />
+        </header>
+
+      </ProfileDetails>
     </ProfileContainer>
   )
 }
--- src/pages/Home/components/Profile/styles.ts
+++ src/pages/Home/components/Profile/styles.ts
@@ -17,3 +17,8 @@ export const ProfilePicture = styled.img`
   border-radius: 8px;
   object-fit: cover;
 `
+export const ProfileDetails = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+`
