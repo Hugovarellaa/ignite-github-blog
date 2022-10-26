ee19bb6b99f43c23877d1d9480eeb1e7a4d2a68b
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-10-26 10:29:11 -0300

    Styled-components Config + defaultTheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 import { Router } from "./Router";
-// import { defaultTheme } from "./styles/theme/defaultTheme";
+import { defaultTheme } from "./styles/theme/defaultTheme";
 
 export function App() {
   return (
-    // <ThemeProvider theme={defaultTheme}>
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
-    // </ThemeProvider>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
