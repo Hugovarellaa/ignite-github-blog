cbbbe48d21509a8bf88ea628844b917ada5cf1b7
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-10-26 17:04:14 -0300

    Loading in Profile

--- src/pages/Home/components/Profile/index.tsx
+++ src/pages/Home/components/Profile/index.tsx
@@ -3,6 +3,7 @@ import { faBuilding, faUserGroup } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useCallback, useEffect, useState } from "react";
 import { ExternalLink } from "../../../../components/ExternalLink";
+import { Loading } from "../../../../components/Loading";
 import { api } from "../../../../services/axios";
 import { ProfileContainer, ProfileDetails, ProfilePicture } from "./styles";
 
@@ -39,37 +40,45 @@ export function Profile() {
 
   return (
     <ProfileContainer>
-      <ProfilePicture src={profileData.avatar_url} />
+      {
+        isLoading ? (
+          <Loading />
+        ) : (
+          <>
+            <ProfilePicture src={profileData.avatar_url} />
 
-      <ProfileDetails>
-        <header>
-          <h1>{profileData.name}</h1>
-          <ExternalLink text="Github" href={profileData.html_url} target='_blank' />
-        </header>
+            <ProfileDetails>
+              <header>
+                <h1>{profileData.name}</h1>
+                <ExternalLink text="Github" href={profileData.html_url} target='_blank' />
+              </header>
 
-        <p>
-          {profileData.bio}
-        </p>
+              <p>
+                {profileData.bio}
+              </p>
 
-        <ul>
-          <li>
-            <FontAwesomeIcon icon={faGithub} />
-            <span>{profileData.login}</span>
-          </li>
-          {
-            profileData?.company && (
-              <li>
-                <FontAwesomeIcon icon={faBuilding} />
-                {profileData.company}
-              </li>
-            )
-          }
-          <li>
-            <FontAwesomeIcon icon={faUserGroup} />
-            {profileData.followers} seguidores
-          </li>
-        </ul>
-      </ProfileDetails>
+              <ul>
+                <li>
+                  <FontAwesomeIcon icon={faGithub} />
+                  <span>{profileData.login}</span>
+                </li>
+                {
+                  profileData?.company && (
+                    <li>
+                      <FontAwesomeIcon icon={faBuilding} />
+                      {profileData.company}
+                    </li>
+                  )
+                }
+                <li>
+                  <FontAwesomeIcon icon={faUserGroup} />
+                  {profileData.followers} seguidores
+                </li>
+              </ul>
+            </ProfileDetails>
+          </>
+        )
+      }
     </ProfileContainer>
   )
 }
